59560cc28d8c06d528c138f51e5bc05a896aa174
akira.takahashi <rike422@gmail.com>
2016-06-01 21:11:01 +0900

    Remove console.log

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@kadira/storybook';
 
 function loadStories() {
-  console.log("load ")
   require('../src/components/stories/button')
   // require as many stories as you need.
 }
