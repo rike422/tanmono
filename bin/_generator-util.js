1e0ede2be5e2be8dc57ca36d204518615463d969
akira.takahashi <rike422@gmail.com>
2016-06-06 23:27:56 +0900

    Split commands

---
+++ bin/_generator-util.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+
+const process = require('process');
+const nunjucks = require('nunjucks');
+const program = require('commander');
+const changCase = require('change-case');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const TEMPLATE_PATH = path.join(__dirname, '../', 'templates');
+const mkdirp = require('mkdirp');
+
+function render(template, context) {
+  return new Promise(function (resolve) {
+    nunjucks.render(path.join(TEMPLATE_PATH, template.from), context, (err, content) => {
+      if (err != null) {
+        console.error(err);
+        process.exit(1);
+      }
+      mkdir(template.to);
+      writeFile(template.to, content);
+      resolve();
+    });
+  });
+}
+
+function resolveConflict(_template) {
+  return inquirer.prompt({
+    type: 'expand',
+    name: 'choice',
+    message: `Overwrite ${_template.to}? (enter "h" for help) [Ynaqh]`,
+    choices: [
+      {
+        key: 'y',
+        name: 'yes, overwrite',
+        value: 'overwrite',
+      },
+      {
+        key: 'a',
+        name: 'all, Overwrite this one and all next',
+        value: 'overwrite_all',
+      },
+      {
+        key: 'n',
+        name: 'no, do not overwrite',
+        value: 'no',
+      },
+      new inquirer.Separator(),
+      {
+        key: 'q',
+        name: 'Quit this process',
+        value: 'quit',
+      },
+    ],
+  }).then((answer) => answer);
+}
+
+function mkdir(outputPath, cb) {
+  mkdirp(path.join(__dirname, '../', path.dirname(outputPath)));
+}
+
+function writeFile(path, content) {
+  console.log(`write ${path}`);
+  fs.writeFile(path, content);
+}
+module.exports = function (files, context) {
+  files.reduce((promise, template) => {
+    return promise.then((answerHistory) => {
+      var answerHistory = answerHistory || [];
+      if (fs.existsSync(template.to) && (answerHistory.every((history) => history !== 'overwrite_all'))) {
+        return resolveConflict(template).then((answer) => {
+          answerHistory.push(answer.choice);
+          switch (answer.choice) {
+            case 'overwrite':
+            case 'overwrite_all':
+              return render(template, context).then(() => Promise.resolve(answerHistory));
+            case 'no':
+              return Promise.resolve(answerHistory);
+            case 'quit':
+              console.log('Aborting...');
+              process.exit(0);
+              break;
+          }
+        });
+      }
+      return render(template, context).then(() => Promise.resolve(answerHistory));
+    });
+  }, Promise.resolve([])).catch(console.error);
+}
