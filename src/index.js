d65f455745b2299a820d5ff89959c85a412edc9f
akira.takahashi <rike422@gmail.com>
2016-06-06 12:40:59 +0900

    Cleanup code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import { Button } from './components/button/button.js';
 
-(function(win) {
-  if(win._tanmonoLoaded) return;
-  else win._tanmonoLoaded = true;
-  win.Button = Button;
-})(window);
-
 module.exports = {
   Button,
 };
