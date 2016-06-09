3ffb31eaf6018c38e19a91469428a025d72c3b5b
akira.takahashi <rike422@gmail.com>
2016-06-09 22:40:24 +0900

    Remove mock component

--- src/components/stories/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf, action } from '@kadira/storybook';
-import Button from '../button/button';
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button type={"default"}>test</Button>
-  ))
-
