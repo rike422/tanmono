e4896954e065e4a6d620b52fc8f50dce1601c5e8
akira.takahashi <rike422@gmail.com>
2016-06-21 13:12:58 +0900

    Add style for Button:Disabled

--- src/components/stories/button.jsx
+++ src/components/stories/button.jsx
@@ -3,7 +3,7 @@ import { storiesOf } from "@kadira/storybook";
 import Button from "../button/button";
 
 storiesOf('Button', module)
-.add('button', function() {
+.add('Buttons', function() {
   return (
   <section>
     <h2> Exmaple(nomal)</h2>
@@ -12,6 +12,19 @@ storiesOf('Button', module)
       <Button type={"success"}>test</Button>
       <Button type={"warning"}>test</Button>
       <Button type={"danger"}>test</Button>
+    </div>
+  </section>
+  );
+})
+.add('Buttons/Disabled', function() {
+  return (
+  <section>
+    <h2> Exmaple(nomal)</h2>
+    <div>
+      <Button type={"default"} disabled={true}>test</Button>
+      <Button type={"success"} disabled={true}>test</Button>
+      <Button type={"warning"} disabled={true}>test</Button>
+      <Button type={"danger"} disabled={true}>test</Button>
     </div>
   </section>
   )
