b573e129170b7b764a552cac4d3513189d74cf0c
akira.takahashi <rike422@gmail.com>
2016-06-24 02:08:15 +0900

    Format

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import CSSModules from "react-css-modules";
-import ButtonStyle from "./style.scss";
-import classnames from "classnames";
+import React from 'react';
+import CSSModules from 'react-css-modules';
+import ButtonStyle from './style.scss';
+import classnames from 'classnames';
 const { PropTypes, Component } = React;
 
 const BUTTON_TYPES = ['default', 'primary', 'success', 'warning', 'danger'];
@@ -9,53 +9,54 @@ const BUTTON_SIZES = ['default', 'large'];
 
 @CSSModules(ButtonStyle, { allowMultiple: true })
 export default class Button extends Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  
+
   static displayName = 'Button';
-  
+
   static propTypes = {
     disabled: PropTypes.bool,
     text: PropTypes.string,
     type: PropTypes.oneOf(BUTTON_TYPES),
+    size: PropTypes.oneOf(BUTTON_SIZES),
     submit: PropTypes.bool,
     loading: PropTypes.bool,
-    preventFocusStyleForTouchAndClick: PropTypes.bool
+    preventFocusStyleForTouchAndClick: PropTypes.bool,
   };
-  
+
   static defaultProps = {
     disabled: false,
     loading: false,
     type: 'default',
     size: 'default',
   };
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   componentWillMount() {
   }
-  
-  componentWillReceiveProps(properties) {
+
+  componentWillReceiveProps() {
   }
-  
+
   componentDidUpdate() {
   }
-  
+
   componentWillUnmount() {
   }
-  
+
   render() {
-    const { text, children, type, size, loading, ...otherProps } = this.props;
-    const style = classnames(type, size, { loading: loading })
+    const { type, size, loading, ...otherProps } = this.props;
+    const style = classnames(type, size, { loading });
     return (
-    <button
-    styleName={style}
-    submit={() => { ::this.props.submit || true } }
-    { ...otherProps }
-    >
-    { this.props.children }
-    </button>
+      <button
+        styleName={style}
+        submit={() => ::this.props.submit || true}
+        { ...otherProps }
+      >
+        {this.props.children}
+      </button>
     );
   }
 }
--- src/components/stories/button.jsx
+++ src/components/stories/button.jsx
@@ -1,31 +1,32 @@
-import React from "react";
-import { storiesOf } from "@kadira/storybook";
-import Button from "../button/button";
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Button from '../button/button';
 
 const ProgressButtonWrapper = React.createClass({
   getInitialState() {
     return {
       button1: false,
-      button2: false
+      button2: false,
     };
   },
+
   render() {
     return (
-    <section>
-      <h2>Sizes</h2>
-      <div>
-        <Button type={"warning"}
-                onClick={() => this.setState({ 'button1': !this.state.button1 })}
-                loading={this.state.button1}>
-          Defalut Button
-        </Button>
-        <Button type={"danger"}
-                onClick={() => this.setState({ 'button2': !this.state.button2 })}
-                loading={this.state.button2} size={"large"}>
-          Large Button
-        </Button>
-      </div>
-    </section>
+      <section>
+        <h2>Sizes</h2>
+        <div>
+          <Button type={'warning'}
+                  onClick={() => this.setState({ button1: !this.state.button1 })}
+                  loading={this.state.button1}>
+            Defalut Button
+          </Button>
+          <Button type={'danger'}
+                  onClick={() => this.setState({ button2: !this.state.button2 })}
+                  loading={this.state.button2} size={'large'}>
+            Large Button
+          </Button>
+        </div>
+      </section>
     );
   }
 });
@@ -33,42 +34,42 @@ const ProgressButtonWrapper = React.createClass({
 storiesOf('Button', module)
 .add('Buttons', function() {
   return (
-  <div>
-    <section>
-      <h2> Colors </h2>
-      <div>
-        <Button type={"default"}>test</Button>
-        <Button type={"success"}>test</Button>
-        <Button type={"warning"}>test</Button>
-        <Button type={"danger"}>test</Button>
-      </div>
-    </section>
-    <section>
-      <h2>Sizes</h2>
-      <div>
-        <Button type={"warning"}>Defalut Button</Button>
-        <Button type={"danger"} size={"large"}>Large Button</Button>
-      </div>
-    </section>
-  </div>
+    <div>
+      <section>
+        <h2> Colors </h2>
+        <div>
+          <Button type={'default'}>test</Button>
+          <Button type={'success'}>test</Button>
+          <Button type={'warning'}>test</Button>
+          <Button type={'danger'}>test</Button>
+        </div>
+      </section>
+      <section>
+        <h2>Sizes</h2>
+        <div>
+          <Button type={'warning'}>Defalut Button</Button>
+          <Button type={'danger'} size={'large'}>Large Button</Button>
+        </div>
+      </section>
+    </div>
   );
 })
 .add('Buttons/Progress', function() {
   return (
-  <ProgressButtonWrapper></ProgressButtonWrapper>
+    <ProgressButtonWrapper></ProgressButtonWrapper>
   );
 })
 .add('Buttons/Disabled', function() {
   return (
-  <section>
-    <h2> Exmaple(nomal)</h2>
-    <div>
-      <Button type={"default"} disabled={true}>test</Button>
-      <Button type={"success"} disabled={true}>test</Button>
-      <Button type={"warning"} disabled={true}>test</Button>
-      <Button type={"danger"} disabled={true}>test</Button>
-    </div>
-  </section>
+    <section>
+      <h2> Exmaple(nomal)</h2>
+      <div>
+        <Button type={'default'} disabled={true}>test</Button>
+        <Button type={'success'} disabled={true}>test</Button>
+        <Button type={'warning'} disabled={true}>test</Button>
+        <Button type={'danger'} disabled={true}>test</Button>
+      </div>
+    </section>
   );
 });
 
