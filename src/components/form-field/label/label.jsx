868702790a8bcb067be15829f734f8efad2c19e4
akira.takahashi <rike422@gmail.com>
2016-07-15 17:06:29 +0900

    Add lable skelton

---
+++ src/components/form-field/label/label.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import CSSModules from 'react-css-modules';
+import LabelStyle from './style.scss';
+const { Component, PropTypes } = React;
+
+@CSSModules(LabelStyle)
+export default class Label extends Component {
+
+  static displayName = 'Label';
+
+  static propTypes = {
+    id: PropTypes.string,
+    name: PropTypes.string,
+    label: PropTypes.string,
+    formFor: PropTypes.string,
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div styleName="label" />
+    );
+  }
+}
