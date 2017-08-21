1fca241281f85b6813a91cbf43660b7e4e256618
akira.takahashi <rike422@gmail.com>
2017-08-21 11:02:30 +0900

    Create style directory

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Grid from './components/grid/grid';
 import Layout from './components/layout/layout';
 import Row from './components/row/row';
 
-require('./style/common.scss');
+import ('./style/common.scss');
 
 module.exports = {
   Button,
