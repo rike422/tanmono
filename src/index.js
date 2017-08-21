eb288d4eec0154ee8a13ee2c9bd40f9340a60601
akira.takahashi <rike422@gmail.com>
2017-08-21 12:56:57 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Button from './components/button/button';
-require('./common');
+import Col from './components/col/col';
+import Grid from './components/grid/grid';
+import Layout from './components/layout/layout';
+import Row from './components/row/row';
+
+require('./style/common.scss');
 
 module.exports = {
   Button,
+  Col,
+  Row,
+  Grid,
+  Layout,
 };
