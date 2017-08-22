47afd88576eeda9c43a3ece7af1ad2d1260e4c31
akira.takahashi <rike422@gmail.com>
2017-08-22 14:54:00 +0900

    Fix import path

--- test/components/layout/footer/footer_spec.jsx
+++ test/components/layout/footer/footer_spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import assert from 'power-assert';
-import Footer from 'components/footer/footer';
-import FooterStyle from 'components/footer/style';
+import Footer from 'components/layout/footer/footer';
+import FooterStyle from 'components/layout/footer/style';
 import { shallow } from 'enzyme';
 
 describe('<Footer>', () => {
